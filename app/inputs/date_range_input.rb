8031ad110c0c9d8312b2d87eca7a7f1d5b5ef53b
Arseniy Pelipas <pelipas@e-garant.ru>
2021-01-25 18:38:13 +0300

    Fix DateRangeInput

--- app/inputs/date_range_input.rb
+++ app/inputs/date_range_input.rb
@@ -1,5 +1,5 @@
 class DateRangeInput < ActiveAdmin::Inputs::Filters::DateRangeInput
-  def input_html_options
-    super.except(:maxlength, :size).merge({ autocomplete: 'off' })
+  def input_html_options(*args)
+    super(*args).except(:maxlength, :size).merge({ autocomplete: 'off' })
   end
 end
